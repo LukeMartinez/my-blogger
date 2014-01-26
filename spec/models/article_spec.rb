38999cfaf8dac8d1d4e119b34342a0e6faebd567
Luke Martinez <lukemartinez@gmail.com>
2014-01-26 14:37:31 -0700

    deleted redundant tests for validations

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,17 +2,6 @@ require 'spec_helper'
 
 describe Article do
 
-  it "can have a title and a body" do
-    article = Article.create(title: "hey", body: "body")
-    expect(article.body).to eq("body")
-    expect(article.title).to eq("hey")
-  end
-
-  it "is not valid without a title" do
-    article = Article.create(body: "hey")
-    expect(article).to_not be_valid
-  end
-
   it "is not valid without a title and body" do
     article = Article.create(title: "hey")
     article2 = Article.create(title: "hey", body: "this is a body")
